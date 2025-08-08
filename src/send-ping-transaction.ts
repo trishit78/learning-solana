f8219226e093b228cc6f9ba1d8f7bcb60e0d78ff
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-08-08 08:14:05 +0530

    used custom onchain programs

---
+++ src/send-ping-transaction.ts
@@ -0,0 +1,57 @@
+import { airdropIfRequired, getKeypairFromEnvironment } from "@solana-developers/helpers";
+import * as web3 from  "@solana/web3.js";
+
+import 'dotenv/config'
+
+const payer = getKeypairFromEnvironment("SECRET_KEY");
+const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
+
+
+const PING_PROGRAM_ADDRESS = "ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa";
+const PING_PROGRAM_DATA_ADDRESS =
+  "Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod";
+
+
+
+
+
+    
+    const newBalance = await airdropIfRequired(
+        connection,
+        payer.publicKey,
+        1* web3.LAMPORTS_PER_SOL,
+        0.5*web3.LAMPORTS_PER_SOL
+    );
+    
+    console.log(newBalance)
+    
+    const transaction = new web3.Transaction();
+    const programId = new web3.PublicKey(PING_PROGRAM_ADDRESS);
+    const pingProgramDataId = new web3.PublicKey(PING_PROGRAM_DATA_ADDRESS);
+    
+    
+const instruction = new web3.TransactionInstruction({
+    keys:[
+        {
+            pubkey:pingProgramDataId,
+            isSigner:false,
+            isWritable:true,
+        },
+    ],
+    programId
+});
+
+
+
+transaction.add(instruction);
+
+const signature = await web3.sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [payer],
+);
+
+console.log(`Transaction completed with signature ${signature}`)
+
+
+
